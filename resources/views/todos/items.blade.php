51cc20948f494c986447b9f085fc5ae31916e5db
aungmyokhant <aungmyokhant2465@gmail.com>
2020-06-13 12:40:26 +0630

    First

---
+++ resources/views/todos/items.blade.php
@@ -0,0 +1,20 @@
+<div class="row mt-3">
+    <div class="col-12">
+        <table class="table">
+            <tbody>
+                <tr>
+                    <td>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend mr-4">
+                                <div class="input-group">
+                                    <input type="checkbox">
+                                </div>
+                            </div>
+                            <span>Wakeup At 5:30 AM</span>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
